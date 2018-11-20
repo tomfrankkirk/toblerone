e096bc06e6d7ad32c65ac63a08a3c06358c599ec
Tom Kirk <thomas.kirk@eng.ox.ac.uk>
2018-11-20 13:19:23 +0000

    wrapping more methods
    Former-commit-id: 17575f34fe0e26ff16474fa5e8a6fe21723249ad
    Former-commit-id: 25c67936d43317ad449bef918da5466ce12c227b

--- ctoblerone.pyx
+++ ctoblerone.pyx
@@ -4,7 +4,9 @@ import cython
 
 
 cdef extern from "tribox.h":
-    char triBoxOverlap(const float boxcenter[3], const float boxhalfsize[3], const float triverts[3][3]); 
+    char triBoxOverlap(const float boxcenter[3], const float boxhalfsize[3], const float triverts[3][3])
+
+    char tribox_wrapper(float bc1, float bc2, float bc3, float fs1, float fs2, float fs3, float tv11, float tv12, float tv13, float tv21, float tv22, float tv23, float tv31, float tv32, float tv33)
 
 cdef extern from "ctoblerone.h":
     void ctestManyRayTriangleIntersections(const int* triangles, const float* points, const float* start, const int nTris, const int ax1, const int ax2, char* results)
@@ -15,50 +17,81 @@ cdef extern from "ctoblerone.h":
 
     void triPlaneIntersections(const float* points, const int* tris, int nTris, const float* testPnt,const float* ray, int normDF, float* output)
 
+    void testManyTriangleVoxelIntersections(int *tris, float *points, float *vC, float *hS, int nTris, char *results)
+
 
 @cython.boundscheck(False) 
 @cython.wraparound(False) 
-def _ctestTriangleVoxelIntersection(voxCent, voxSize, tri):
+def _ctestTriangleVoxelIntersection(voxCent, halfSize, tri):
+    """WARNING: this function expects voxel half size, not full size, 
+    as is the case for _cyfilterTriangles()"""
 
     cdef float[:] vC = voxCent.flatten()
-    cdef float[:] halfSize = voxSize
-    for i in range(3):
-        halfSize[i] = halfSize[i]/2 
+    cdef float[:] hS = halfSize.flatten()
     cdef float verts[3][3]
     for i in range(3):
         verts[0][i] = tri[0,i]
         verts[1][i] = tri[1,i]
         verts[2][i] = tri[2,i]
 
-    return triBoxOverlap(&vC[0], &halfSize[0], verts)
+    return bool(triBoxOverlap(&vC[0], &hS[0], &verts[0]))
+
 
 @cython.boundscheck(False) 
 @cython.wraparound(False) 
-def _cfilterTriangles(tris, 
-                      points, 
-                      voxCent, 
-                      voxSize):
-
-    cdef np.ndarray[char, ndim=1, cast=True] fltr = \
-        np.zeros(tris.shape[0], dtype=np.bool)
+def _cfilterTriangles(tris, points, vC, vS):
+    
+    cdef float[:] voxCent = vC 
     cdef np.ndarray[int, ndim=1] ts = tris.flatten()
-    cdef np.ndarray[np.float32_t, ndim=1] ps = points.flatten()
+    cdef np.ndarray[float, ndim=1] ps = points.flatten()
+    cdef np.ndarray[char, ndim=1, cast=True] fltr = np.zeros(tris.shape[0], dtype=bool)
     cdef int nTris = tris.shape[0]
-    cdef float[:] halfSize = voxSize
+    cdef float[:] halfSize = vS
     for i in range(3):
         halfSize[i] = halfSize[i]/2 
-    cdef float[:] cent = voxCent.flatten()
 
-    testTrianglesVoxelIntersection(&ts[0], &ps[0], nTris, 
-        &cent[0], &halfSize[0], &fltr[0]); 
+    testManyTriangleVoxelIntersections(
+        &ts[0], &ps[0], &voxCent[0], &halfSize[0], nTris, &fltr[0]
+    )
 
-    return fltr
+    return fltr 
 
+@cython.boundscheck(False) 
+@cython.wraparound(False) 
+def _cyfilterTriangles(tris, points, vC, vS):
+    
+    cdef Py_ssize_t t, a, b, c, i 
+    cdef np.ndarray[char, ndim=1, cast=True] fltr = \
+        np.zeros(tris.shape[0], dtype=bool)
+    
+    cdef np.ndarray[float, ndim=1] voxCent = vC.flatten() 
+    cdef np.ndarray[float, ndim=1] halfSize = vS.flatten() 
+    cdef float tri[3][3]
+    for i in range(3):
+        halfSize[i] = halfSize[i]/2 
+
+    for t in range(tris.shape[0]):
+        a = tris[t,0]
+        b = tris[t,1]
+        c = tris[t,2]
+
+        for i in range(3):
+            tri[0][i] = points[a,i]
+            tri[1][i] = points[b,i]
+            tri[2][i] = points[c,i]
+
+        fltr[t] = triBoxOverlap(&voxCent[0], &halfSize[0], &tri[0])     
+
+    return fltr 
 
 
 @cython.boundscheck(False) 
 @cython.wraparound(False) 
-def _cytestManyRayTriangleIntersections(int[:,:] tris, float[:,:] points, float[:] start, int ax1, int ax2):
+def _cytestManyRayTriangleIntersections(int[:,:] tris, float[:,:] points, start, int ax1, int ax2):
+
+    # cdef np.ndarray[int, ndim=1] ts = tris.flatten()
+    # cdef np.ndarray[float, ndim=1] ps = points.flatten()
+    cdef np.ndarray[float, ndim=1] st = start.flatten()
 
     cdef np.ndarray[char, ndim=1, cast=True] fltr = \
         np.zeros(tris.shape[0], dtype=np.bool)
@@ -76,9 +109,23 @@ def _cytestManyRayTriangleIntersections(int[:,:] tris, float[:,:] points, float[
             tri[1][i] = points[b,i]
             tri[2][i] = points[c,i]
 
-        fltr[t] = testRayTriangleIntersection(tri, &start[0], ax1, ax2)     
+        fltr[t] = testRayTriangleIntersection(tri, &st[0], ax1, ax2)     
 
     return fltr 
+
+def tribox(float bc1, float bc2, float bc3, 
+        float fs1, float fs2, float fs3,
+        float tv11, float tv12, float tv13,
+        float tv21, float tv22, float tv23,
+        float tv31, float tv32, float tv33):
+
+    
+    return tribox_wrapper( bc1,  bc2,  bc3, 
+                    fs1,  fs2,  fs3,
+                    tv11,  tv12,  tv13,
+                    tv21,  tv22,  tv23,
+                    tv31,  tv32,  tv33)
+
 
 
 @cython.boundscheck(False) 
@@ -99,7 +146,7 @@ def _ctestManyRayTriangleIntersections(tris, points, float[:] start,
 
 # Cython method: loop over triangles in cython and do the maths there. 
 def _cyfindRayTriPlaneIntersections(float[:,:] points, int[:,:] tris, 
-    float[:] testPnt, float[:] ray, int normDF):
+    float[:] testPnt, float[:] ray):
 
     cdef Py_ssize_t t, a, b, c, d 
     cdef Py_ssize_t nTris = tris.shape[0]
@@ -136,7 +183,8 @@ def _cfindRayTriPlaneIntersections(points, tris, float[:] testPnt, float[:] ray,
 
     triPlaneIntersections(&ps[0], &ts[0], nTris, &testPnt[0], &ray[0], normDF, &fltr[0])
     return fltr 
-    
+
+
 
 
 
