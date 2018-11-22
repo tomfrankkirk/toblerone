75f4fe74501a324cd4ddba67aa871c371d2fabed
Tom <tom@Toms-MacBook-Pro.local>
2018-11-22 23:10:38 +0000

    tidying up c files
    Former-commit-id: d4db3cdffcd1eeb498821f1ac9756d0470e33440
    Former-commit-id: 1218f991e3bcea88c5e1075412ac33073f2a15eb

--- extern/ctoblerone.h
+++ extern/ctoblerone.h
@@ -3,17 +3,6 @@
 
 char testRayTriangleIntersection(const float tri[3][3], const float start[3], int ax1, int ax2); 
 
-void testTrianglesVoxelIntersection(const int* triangles, const float* points, int nTris, const float* voxCent, const float* voxHalfSize, char* results); 
+static void cross(const float* v1, const float* v2, float* out); 
 
-void ctestManyRayTriangleIntersections(const int* triangles, const float* points, const float* start, const int nTris, const int ax1, const int ax2, char* results); 
-
-
-char ray_wrapper(float s1, float s2, float s3, 
-                   float tv11, float tv12, float tv13,
-                   float tv21, float tv22, float tv23,
-                   float tv31, float tv32, float tv33, 
-                   int ax1, int ax2); 
-
-void triPlaneIntersections(const float* points, const int* tris, int nTris, const float* testPnt, const float* ray, int normDF, float* output);
-
-void testManyTriangleVoxelIntersections(int *tris, float *points, float *vC, float *hS, int nTris, char *results); 
+static float dot(const float* v1, const float* v2); 
