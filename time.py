c6185c7eb4a5f4ebced68b82596bac65bc6bb75f
Tom Kirk <thomas.kirk@eng.ox.ac.uk>
2018-11-23 12:03:10 +0000

    removed old args
    Former-commit-id: b386fa6d946505379afccdbb54d969c738dd64bb

--- time.py
+++ time.py
@@ -11,27 +11,11 @@ import cProfile
 import re 
 
 def doToblerone(): 
-    if False:
-        ref = 'E:/HCP100/references/reference1.0.nii'
-        LWS = 'E:/HCP100/103818/T1w/Native/103818.L.white.native.surf.gii'
-        LPS = 'E:/HCP100/103818/T1w/Native/103818.L.pial.native.surf.gii'
-        RWS = 'E:/HCP100/103818/T1w/Native/103818.R.white.native.surf.gii'
-        RPS = 'E:/HCP100/103818/T1w/Native/103818.R.pial.native.surf.gii'
-
-    else:
-        # ref = '/Users/tom/Data/HCPExampleData/103818/T1w/Processed/ref1.0.nii'
-        # LWS = '/Users/tom/Data/HCPExampleData/103818/T1w/Natpyive/103818.L.white.native.surf.gii'
-        # LPS = '/Users/tom/Data/HCPExampleData/103818/T1w/Native/103818.L.pial.native.surf.gii'
-        pass
-
-    ref = 'testdata/perfusionNative1.nii'
+    ref = 'testdata/FS/aslref.nii'
+        
     s2r = np.identity(4)
-    outDir = 'testdata'
-    outName = 'py_test_tob_1.0'
-    t.estimatePVs(ref=ref, FSdir='testdata',
-        struct2ref=s2r, outdir=outDir, name=outName, \
-        saveassocs=True)
-
+    t.estimatePVs(ref=ref, FSdir='testdata/FS',
+        struct2ref=s2r, saveassocs=True)
 
 if __name__ == '__main__':
     doToblerone()
