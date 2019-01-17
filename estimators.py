475e57d431a387b5b05b860ea94e82df8d6c2bc6
Tom Kirk <thomas.kirk@eng.ox.ac.uk>
2019-01-17 13:43:58 +0000

    created
    Former-commit-id: 30f74f0d718c37e69b5b5116d6cf350c04c48f74

---
+++ estimators.py
@@ -0,0 +1,148 @@
+import functools
+import multiprocessing
+import warnings
+
+import numpy as np
+
+from . import toblerone
+from . import pvcore 
+from .classes import Hemisphere, Structure, Surface, Patch
+
+
+
+def cortex(hemispheres, refSpace, supersampler, cores):
+
+    surfs = [ s for h in hemispheres for s in h.surfs() ]
+    FoVoffset, FoVsize = toblerone._determineFullFoV(surfs, refSpace)
+
+    for s in surfs:
+        s.shiftFoV(FoVoffset, FoVsize)
+        s.formAssociations(FoVsize, cores)
+        s.calculateXprods()
+
+    # Prepare for estimation. Generate list of voxels to process:
+    # Start with grid, add offset, then flatten to linear indices. 
+    supersampler = np.ceil(refSpace.voxSize).astype(np.int8)
+    print("Supersampling factor set at:", supersampler)
+    voxList = pvcore.getVoxList(refSpace.imgSize, FoVoffset, FoVsize)
+    
+    # Fill in whole voxels (ie, no PVs), then match the results of the map
+    # to respective surfaces.
+    print("Filling whole voxels")
+    voxelise = functools.partial(toblerone.voxelise, FoVsize)
+    if cores > 1: 
+        with multiprocessing.Pool(min([cores, len(surfs)])) as p:
+            fills = p.map(voxelise, surfs)
+    else: 
+        fills = list(map(voxelise, surfs))
+    [ setattr(s, 'voxelised', f) for (s,f) in zip(surfs, fills) ]
+        
+    # Estimate fractions for each surface
+    print("Estimating fractions")
+    for h in hemispheres:
+        if np.any(np.max(np.abs(h.inSurf.points)) > 
+            np.max(np.abs(h.outSurf.points))):
+            raise RuntimeWarning("Inner surface vertices appear to be further",\
+                "from the origin than the outer vertices. Are the surfaces in",\
+                "the correct order?")
+        
+        for s, d in zip(h.surfs(), ['in', 'out']):
+            descriptor = " {} cortex {}".format(h.side, d)
+            s.flist = np.intersect1d(voxList, s.LUT).astype(np.int32)
+            f = toblerone._estimateFractions(s, FoVsize, supersampler, 
+                s.flist, descriptor, cores)
+            s.fractions = f 
+
+    for h in hemispheres:
+        inFractions = (h.inSurf.voxelised).astype(np.float32)
+        outFractions = (h.outSurf.voxelised).astype(np.float32)
+        inFractions[h.inSurf.vlist] = h.inSurf.fractions
+        outFractions[h.outSurf.vlist] = h.outSurf.fractions
+
+        # Combine estimates from each surface into whole hemi PV estimates
+        hemiPVs = np.zeros((np.prod(FoVsize), 3), dtype=np.float32)
+        hemiPVs[:,1] = inFractions 
+        hemiPVs[:,0] = np.maximum(0.0, outFractions - inFractions)
+        hemiPVs[:,2] = 1.0 - np.sum(hemiPVs[:,0:2], axis=1)
+
+        # And write into the hemisphere object. 
+        h.PVs = hemiPVs
+
+
+    # Merge the fill masks by giving priority to GM, then WM, then CSF.
+    if len(hemispheres) == 1:
+        outPVs = hemispheres[0].PVs
+    else:
+        h1, h2 = hemispheres
+        outPVs = np.zeros((np.prod(FoVsize), 3), dtype=np.float32)
+        outPVs[:,0] = np.minimum(1.0, h1.PVs[:,0] + h2.PVs[:,0])
+        outPVs[:,1] = np.minimum(1.0 - outPVs[:,0], \
+            h1.PVs[:,1] + h2.PVs[:,1])
+        outPVs[:,2] = 1.0 - np.sum(outPVs[:,0:2], axis=1)
+
+    # Sanity checks
+    if np.any(outPVs > 1.0): 
+        raise RuntimeError("PV exceeds 1")
+
+    if np.any(outPVs < 0.0):
+        raise RuntimeError("Negative PV returned")
+
+    if not np.all(np.sum(outPVs, axis=1) == 1.0):
+        raise RuntimeError("PVs do not sum to 1")
+
+    # Form the surface mask (3D logical) as any voxel containing GM or 
+    # intersecting the cortex (these definitions should always be equivalent)
+    assocsMask = np.zeros((outPVs.shape[0], 1), dtype=bool)
+    for h in hemispheres:
+        for s in h.surfs(): 
+            assocsMask[s.LUT] = True
+    assocsMask[outPVs[:,0] > 0] = True 
+
+    # Reshape images back into 4D or 3D images
+    outPVs = np.reshape(outPVs, (FoVsize[0], FoVsize[1], \
+        FoVsize[2], 3))
+    assocsMask = np.reshape(assocsMask, tuple(FoVsize[0:3]))
+
+    # Extract the output within the FoV of the reference image
+    outPVs = outPVs[ FoVoffset[0] : FoVoffset[0] + refSpace.imgSize[0], \
+        FoVoffset[1] : FoVoffset[1] + refSpace.imgSize[1], \
+        FoVoffset[2] : FoVoffset[2] + refSpace.imgSize[2] ]
+    assocsMask = outPVs[ FoVoffset[0] : FoVoffset[0] + refSpace.imgSize[0], \
+        FoVoffset[1] : FoVoffset[1] + refSpace.imgSize[1], \
+        FoVoffset[2] : FoVoffset[2] + refSpace.imgSize[2] ]
+
+    return outPVs, assocsMask
+
+
+def structure(refSpace, cores, supersampler, struct):
+
+    surf = struct.surf
+    surf.calculateXprods()
+
+    FoVoffset, FoVsize = toblerone._determineFullFoV([surf], refSpace)
+    surf.shiftFoV(FoVoffset, FoVsize)
+    surf.formAssociations(FoVsize, cores)
+
+    # Prepare for estimation. Generate list of voxels to process:
+    # Start with grid, add offset, then flatten to linear indices. 
+    voxList = pvcore.getVoxList(refSpace.imgSize, FoVoffset, FoVsize)
+    vlist = np.intersect1d(voxList, surf.LUT).astype(np.int32)
+    if not vlist.size:
+        warnings.warn("Surface {} does not lie within reference image"
+            .format(struct.name))
+
+    surf.voxelised = toblerone.voxelise(FoVsize, surf)
+    desc = '' 
+    fractions = toblerone._estimateFractions(surf, FoVsize, 
+        supersampler, vlist, desc, cores)
+
+    outPVs = surf.voxelised.astype(np.float32)
+    outPVs[vlist] = fractions 
+    outPVs = outPVs.reshape(FoVsize[0], FoVsize[1], FoVsize[2])
+
+    # Extract the output within the FoV of the reference image
+    outPVs = outPVs[ FoVoffset[0] : FoVoffset[0] + refSpace.imgSize[0], \
+        FoVoffset[1] : FoVoffset[1] + refSpace.imgSize[1], \
+        FoVoffset[2] : FoVoffset[2] + refSpace.imgSize[2] ]
+
+    return outPVs
