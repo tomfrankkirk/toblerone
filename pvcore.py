250528a7ed5837bd5d7e3869cc41b81d1aaaab5f
Tom Kirk <thomas.kirk@eng.ox.ac.uk>
2019-01-17 13:45:08 +0000

    moved out classes
    Former-commit-id: 350c7734a82ee1494d68745f529a3eef8b10acc5
    Former-commit-id: 278bc0dc64ff78b27be81023b1d5a950460fe9cd

--- pvcore.py
+++ pvcore.py
@@ -1,104 +1,17 @@
-import nibabel
-import os.path as op
-import numpy as np
+import sys
 import copy
 import itertools
+import shutil
+import os.path as op
+import os
+import subprocess
 
+import numpy as np
+import nibabel
+import scipy.ndimage
 
-# Class definitions -----------------------------------------------------------
-
-# Class to contain an images voxel grid, including dimensions and vox2world transforms
-class ImageSpace(object):
-
-    def __init__(self, imgSize, voxSize, vox2world):
-        """Use the static method .fromfile() to initialise"""
-        self.imgSize = imgSize
-        self.voxSize = voxSize
-        self.vox2world = vox2world
-        self.world2vox = np.linalg.inv(vox2world)
-        
-
-    @staticmethod
-    def fromfile(path):
-        """Load the image at path and read in header matrix, image size 
-        and voxel size
-        """
-
-        if not op.isfile(path):
-            raise RuntimeError("Image does not exist")
-
-        img = nibabel.load(path)
-        return ImageSpace(
-            img.header['dim'][1:4], 
-            img.header['pixdim'][1:4], 
-            img.affine
-        )
-
-
-    def supersample(self, factor):
-        """Produce a new image space which is a copy of the current space, 
-        supersampled by a factor of (a,b,c) in each dimension 
-
-        Args:
-            factor: tuple/list of length 3, ints in each image dimension
-        
-        Returns: 
-            new image space
-        """
-
-        if not len(factor) == 3:
-            raise RuntimeError("Factor must have length 3")
-
-        newSize = self.imgSize * factor
-        newVoxSize = self.voxSize / factor
-        newVox2World = copy.deepcopy(self.vox2world)
-        for r in range(3):
-            newVox2World[r, 0:3] = newVox2World[r, 0:3] / factor[r]
-
-        # The direction vector travelled in incrementing each index 
-        # within the original grid by 1 is: 
-        ijkvec = np.sum(self.vox2world[0:3,0:3], axis=1)
-
-        # Origin of the src voxel grid is at (NB 1 is dist between vox cents)
-        orig = self.vox2world[0:3,3] - (0.5 * ijkvec)
-
-        # Which means the new grid has centre coord of vox (0,0,0) at: 
-        offset = orig + (0.5 * ijkvec / factor)
-        newVox2World[0:3,3] = offset 
-
-        # Check the bounds of the new voxel grid we have created
-        svertices = np.array(list(itertools.product([-0.5, newSize[0] - 0.5], 
-            [-0.5, newSize[1] - 0.5], [-0.5, newSize[2] - 0.5])))
-        rvertices = np.array(list(itertools.product([-0.5, self.imgSize[0] - 0.5], 
-            [-0.5, self.imgSize[1] - 0.5], [-0.5, self.imgSize[2] - 0.5])))
-        rvertices = _affineTransformPoints(rvertices, self.vox2world)
-        svertices = _affineTransformPoints(svertices, newVox2World)
-        assert np.all(np.abs(rvertices -svertices) < 1e-6)
-
-        return ImageSpace(newSize, newVoxSize, newVox2World)
-
-
-    def saveImage(self, data, path):
-
-        if not np.all(data.shape[0:3] == self.imgSize):
-            raise RuntimeError("Data size does not match image size")
-
-        img = nibabel.Nifti2Image(data, self.vox2world)
-        nibabel.save(img, path)
-
-
-# Function definitions --------------------------------------------------------
-
-
-def _addSuffixToFilename(suffix, fname):
-    """Add suffix to filename, whilst preserving original extension"""
-    root = copy.copy(fname)
-    ext = ''
-    while '.' in root:
-        root, e = op.splitext(root)
-        ext = e + ext 
-    
-    return root + suffix + ext 
+from pvtools.classes import ImageSpace, TISSUES
+from pvtools import fileutils
 
 
 def _affineTransformPoints(points, affine):
@@ -120,24 +33,6 @@ def _affineTransformPoints(points, affine):
     return (transfd[0:3,:]).T
 
 
-def _loadSurfsToDict(FSdir):
-    sdir = op.realpath(op.join(FSdir, 'surf'))
-
-    if not op.isdir(sdir):
-        raise RuntimeError("Subject's surf directory does not exist")
-
-    surfs = {}    
-    for s in ['LWS', 'LPS', 'RWS', 'RPS']:
-        snames = {'L': 'lh', 'R': 'rh'}
-        exts = {'W': '.white', 'P': '.pial'}
-        surfs[s] = op.join(sdir, snames[s[0]] + exts[s[1]])
-
-    if not all(map(op.isfile, surfs.values())):
-        raise RuntimeError("One of the subject's surfaces does not exist")
-
-    return surfs
-
-
 def _coordinatesForGrid(ofSize):
     I, J, K = np.unravel_index(np.arange(np.prod(ofSize)), ofSize)
     cents = np.vstack((I.flatten(), J.flatten(), K.flatten())).T
@@ -153,7 +48,6 @@ def _maskVolumes(fourDarray, mask):
         fourDarray[:,:,:,n] = (fourDarray[:,:,:,n] * mask)
 
     return fourDarray
-
 
 
 def _adjustFLIRT(source, reference, transform):
@@ -220,3 +114,284 @@ def _adjustFLIRT(source, reference, transform):
         transform), srcSpace)
     return np.matmul(outAff, np.linalg.inv(srcAff))
 
+
+def _sysprint(txt):
+    sys.stdout.write(txt)
+    sys.stdout.flush()
+
+
+def _resampleImage(data, srcSpace, destSpace, src2dest):
+    """Resample array data onto destination space, applying affine transformation
+    at the same time
+
+    Args: 
+        data: array to resample
+        scrSpace: ImageSpace in which the data currently exists 
+        destSpace: ImageSpace onto which it should be resampled
+        src2dest: 4x4 transformation matrix to apply during resampling
+
+    Returns: 
+        array of size destSpace.imgSize
+    """
+
+    # Transform the destination grid into world coordinates, aligned with src
+    destvox2src = np.matmul(np.linalg.inv(src2dest), destSpace.vox2world)
+    destvox2src = np.matmul(np.linalg.inv(srcSpace.vox2world), destvox2src)
+
+    # Interpolate. 
+    out = scipy.ndimage.affine_transform(data, destvox2src, 
+        output_shape=destSpace.imgSize, mode='constant', order=3)
+
+    # Due to the spline interpolants, the resampled output can go outside
+    # the original min,max of the input data 
+    out = np.maximum(out, data.min())
+    out = np.minimum(out, data.max())
+
+    return out 
+
+
+def _superResampleImage(source, factor, destSpace, src2dest):
+    """Resample an image onto a new space, applying an affine transformation
+    at the same time. The source image transformed onto an upsampled copy of 
+    the destination space first and then block-summed back down to the required
+    resolution. 
+
+    Args:
+        source: path to image to resample
+        factor: iterable length 3, extent to upsample in each dim 
+        destSpace: an ImageSpace representing the destination space
+        src2dest: affine transformation matrix (4x4) between source and reference
+
+    Returns: 
+        an array of the dimensions given by destSpace.imgSize
+    """
+
+    # Load input data and read in the space for it
+    srcSpace = ImageSpace(source)
+    data = nibabel.load(source).get_fdata().astype(np.float32)
+
+    # Create a supersampled version of the space 
+    superSpace = destSpace.supersample(factor)
+
+    # Resample onto this new grid, applying transform at the same time. 
+    # Then sum the array blocks and divide by the size of each block to 
+    # get the mean value within each block. This is the final output. 
+    resamp = _resampleImage(data, srcSpace, superSpace, src2dest)
+    resamp = _sumArrayBlocks(resamp, factor) / np.prod(factor)
+
+    return resamp
+
+
+def _sumArrayBlocks(array, factor):
+    """Sum sub-arrays of a larger array, each of which is sized according to factor. 
+    The array is split into smaller subarrays of size given by factor, each of which 
+    is summed, and the results returned in a new array, shrunk accordingly. 
+
+    Args:
+        array: n-dimensional array of data to sum
+        factor: n-length tuple, size of sub-arrays to sum over
+
+    Returns:
+        array of size array.shape/factor, each element containing the sum of the 
+            corresponding subarray in the input
+    """
+
+    outshape = [ int(s/f) for (s,f) in zip(array.shape, factor) ]
+    out = np.copy(array)
+
+    for dim in range(3):
+        newshape = [0] * 4
+
+        for d in range(3):
+            if d < dim: 
+                newshape[d] = outshape[d]
+            elif d == dim: 
+                newshape[d+1] = factor[d]
+                newshape[d] = outshape[d]
+            else: 
+                newshape[d+1] = array.shape[d]
+
+        newshape = newshape + list(array.shape[3:])
+        out = np.sum(out.reshape(newshape), axis=dim+1)
+
+    return out 
+
+
+
+def _mergeCortexAndSubcorticalPVs(subcortPath, cortexPath, reference, outpath):
+    """Take subcort PVs in intermediate space and Tob estimates, 
+    sum across grid and output results in reference space"""
+
+    refSpace = ImageSpace(reference)
+    intSpace = ImageSpace(subcortPath)
+    cortex = nibabel.load(cortexPath).get_fdata().astype(np.float32)
+    subcortex = nibabel.load(subcortPath).get_fdata().astype(np.float32)
+
+    # Calculate the factor multiple between spaces. Assert no remainder, 
+    # then force into ints. 
+    factor = [ i/r for (i,r) in zip(intSpace.imgSize, refSpace.imgSize) ]
+    if not all(map(lambda f: f%1 == 0, factor)):
+        raise RuntimeError("Intermediate space is not an integer multiple of reference")
+    factor = [ int(f) for f in factor ]
+
+    # Cortex mask: accept all and(GM, CSF) from Toblerone
+    # Apply mask to the different sets of estimates. 
+    mask = np.logical_or(cortex[:,:,:,0], cortex[:,:,:,2]) 
+    outdir = op.dirname(cortexPath)
+    intSpace.saveImage(mask.astype(np.int8), op.join(outdir, 'mask.nii.gz'))
+
+    for (a, m, f) in zip([cortex, subcortex], [mask, ~mask], 
+        [cortexPath, subcortPath]):
+        a = _maskVolumes(a, m)
+        intSpace.saveImage(a, fileutils._addSuffixToFilename('_masked', f))
+
+    # Combine estimates in intermediate space using the mask
+    print("Combining estimates in intermediate space")
+    summed = cortex + subcortex
+
+    # Sum array blocks and divide by blocksize to get mean within each
+    print("Summing estimates back into reference space")
+    outpvs = _sumArrayBlocks(summed, factor) / np.prod(factor)
+
+    # Rescale by sum of each voxels estimates to get range [0 1]
+    # (small float rounding errors expected otherwise)
+    divisors = np.sum(outpvs, axis=3)
+    for d in range(3):
+        outpvs[:,:,:,d] = outpvs[:,:,:,d] / divisors
+
+    print("Saving final output at:", outpath)
+    refSpace.saveImage(outpvs, outpath)
+
+
+def _estimateIntermediatePVs(subcorts, surfs, reference, struct2ref, 
+    savepaths, tob):
+    """Estimate PVs in intermediate space by resampling subcortical estimates
+    and running Toblerone on surfaces.
+    
+    Args:
+        subcorts:   dict with keys GM/WM/CSF for paths to PV estimates for 
+                        each of these tissues respectively, in structural 
+                        space (ie, FAST)
+        surfs:      dict with keys RWS/RPS/LPS/LWS for paths to each of these
+                        surfaces
+        reference:  path to reference image
+        struct2ref: structural to reference affine transform, in world/world
+                        coordinates (ie, adjusted-FLIRT)
+        savepaths:  two path names, where to save subcortical and cortex 
+                        respectively output. 
+    """
+
+    # Load reference image information, create intermediate space as 
+    # supersampled copy
+    refSpace = ImageSpace(reference)
+    factor = np.maximum(np.floor(refSpace.voxSize), [1,1,1]).astype(np.int8)
+    intSpace = refSpace.supersample(factor)
+    print("Intermediate factor set at", factor)
+
+    # Resample subcortical estimates and flatten into single image. 
+    _sysprint("Resampling subcortical estimates...")
+    subcortPVs = np.stack((_superResampleImage(subcorts[t], (2,2,2), 
+        intSpace, struct2ref) for t in TISSUES ), axis=3) 
+    intSpace.saveImage(subcortPVs, savepaths[0])   
+    _sysprint("Done.\n") 
+    
+    # Run Toblerone to estimate cortex PVs. Use the subcortical image as
+    # the reference for the intermediate space 
+    print("Estimating within cortex")
+    if tob: 
+        pass
+        # cortex.estimatePVs(LWS=surfs['LWS'], LPS=surfs['LPS'], 
+        #     RWS=surfs['RWS'], RPS=surfs['RPS'], ref=savepaths[0], 
+        #     struct2ref=struct2ref, name=savepaths[1])
+
+
+
+def getVoxList(imgSize, FoVoffset, FoVsize):
+    voxSubs = _coordinatesForGrid(imgSize)
+    voxSubs = voxSubs + FoVoffset 
+    return np.ravel_multi_index((voxSubs[:,0], voxSubs[:,1], voxSubs[:,2]),
+        FoVsize)
+
+
+
+def _shellCommand(cmd):   
+    try: 
+        ret = subprocess.run(cmd, stdout=sys.stdout, stderr=sys.stderr, 
+            shell=True)
+        if ret.returncode:
+            print("Non-zero return code")
+            raise RuntimeError()
+    except Exception as e:
+        print("Error when executing cmd:", cmd)
+        raise e
+
+
+def _runFreeSurfer(struct, dir):
+    struct = op.abspath(struct)
+    pwd = os.getcwd()
+    os.chdir(dir)
+    cmd = 'recon-all -i {} -all -subjid fs -sd .'.format(struct)
+    print("Preparing to call FreeSurfer: this will take ~10 hours\n")
+    _shellCommand(cmd)
+    os.chdir(pwd)
+
+
+def _runFIRST(struct, dir):
+    nameroot, _ = fileutils.splitExts(struct)
+    struct = op.abspath(struct)
+    pwd = os.getcwd()
+    os.chdir(dir)
+    cmd = 'run_first_all -i {} -o {} -b'.format(struct, nameroot)
+    print("Preparing to call FIRST\n")
+    _shellCommand(cmd)
+    os.chdir(pwd)
+
+
+def _estimateStructuralPVs(struct, savepaths):
+    """Run FAST and FreeSurfer on the given structural image, saving the
+    output into respective folders within the given directory. 
+
+    Args:
+        struct:     path to structural image
+        savepaths:  dict with keys GM/WM/CSF, paths to save images. 
+        debug:      don't run commands 
+    
+    Returns: 
+        each tool, with the following keys:
+        subcorts: GM, WM, CSF: paths to PV estimates for these tissues
+        surfs: LWS/LPS/RWS/RPS: Left/Right White/Pial surfaces.
+    """
+
+    # Required to find FAST's output later on
+    _, sname = op.split(struct)
+    while '.' in sname:
+        sname, _ = op.splitext(sname)
+
+    # Move into the output dir, run FAST, move back afterwards
+    pwd = os.getcwd()
+    fastdir = op.dirname(savepaths['GM'])
+
+    if not op.isdir(fastdir):
+        os.mkdir(fastdir)
+
+    os.chdir(fastdir)
+    _sysprint("Running FAST...")
+    newfname = op.split(struct)[-1]
+    shutil.copyfile(struct, newfname)
+    cmd = 'fast ' + newfname 
+    _shellCommand(cmd)
+    _sysprint("Done.\n")
+
+    # Convert between FAST's naming convention and explicit tissue names
+    oldname = lambda n: \
+        op.join(sname + '_pve_{}.nii.gz'.format(n))
+    for (n,t) in zip([1,2,0], TISSUES):
+        shutil.copyfile(oldname(n), savepaths[t])
+
+    # spc = pvcore.ImageSpace.fromfile(savepaths['GM'])
+    # wm = nibabel.load(savepaths['WM']).get_fdata()
+    # gm = nibabel.load(savepaths['GM']).get_fdata()
+    # csf = 1 - (wm + gm)
+    # spc.saveImage(csf, savepaths['CSF'])
+
+    os.chdir(pwd)
