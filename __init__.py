212ad0e3ac6d5d2a492f3368bf720dcf86a82a02
Tom Kirk <thomas.kirk@eng.ox.ac.uk>
2019-01-17 13:45:59 +0000

    module interface
    Former-commit-id: d854b48f950c9d38a667eefb2927947527ea9647
    Former-commit-id: a25a2475665fe2ee87a39c54273b40b5a3924886

--- __init__.py
+++ __init__.py
@@ -1,8 +1,7 @@
 """Volumetric- and surface-based partial volume estimation tools"""
 
-from .toblerone import estimatePVs as estimate_cortex
-from .pvtools import estimate_all, merge_with_surface
+from .pvtools import estimate_all, estimate_cortex, estimate_structure
 
-__all__ = ['pvcore', 'pvtools', 'toblerone']
+__all__ = ['pvcore', 'pvtools', 'toblerone', 'classes']
 __author__ = 'Tom Kirk'
 __version__ = '0.1'
