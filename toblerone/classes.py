6a2d9837702bc80b4084ce4c09ad263f3c75450a
Tom Kirk <thomas.kirk@eng.ox.ac.uk>
2019-09-23 15:45:28 +0100

    voxelise() out of core module and into the surface class
    Former-commit-id: f52b1bedebfc717e1d52717be91084ccaf19ca9d
    Former-commit-id: 01f2e22d6e679b2b98238804cec7889b32d32bfc

--- toblerone/classes.py
+++ toblerone/classes.py
@@ -652,17 +652,107 @@ class Surface(object):
         else: 
             return None
 
+    
+    def voxelise(self):
+        """Voxelise (create binary in/out mask) a surface within a voxel grid
+        of size size. Surface coordinates must be in 0-indexed voxel units, 
+        as will the voxel grid be interpreted (ie, 0 : size - 1 in xyz). 
+        Method is defined as static on the class for compataibility with 
+        multiprocessing.Pool().
 
-    def shiftFoV(self, offset, size):
-        """Shift the points of this surface by an offset so that it lies 
-        within the FoV given by size (in voxel coordinates)
+        Args: 
+            None, but surface must have been indexed for a space first 
+
+        Updates: 
+            Surface.voxelised: flat boolean mask of voxels contained
+                 within the surface. Use reshape(size) as required. 
         """
 
-        self.points += offset
-        if np.any(np.round(self.points.min(axis=0)) < 0):
-            raise RuntimeError("FoV offset does not remove negative coordinates")
-        if np.any(np.round(self.points.max(axis=0)) >= size):
-            raise RuntimeError("Full FoV does not contain all surface coordinates")
+        try: 
+
+            # Project rays along the largest dimension to classify as many voxels
+            # at once as we can 
+            size = self.index_space.size 
+            dim = np.argmax(size)
+            mask = np.zeros(np.prod(size), dtype=bool)
+            otherdims = [0,1,2]
+            otherdims.remove(dim)
+            d1, d2 = tuple(otherdims)
+            startPoint = np.zeros(3, dtype=np.float32)
+
+            # The lazy way of working out strides through the voxel grid, 
+            # regardless of what dimension we are projecting rays along. Define
+            # a single ray of voxels, convert to linear indices and calculate 
+            # the stride from that. 
+            allIJKs = np.vstack(np.unravel_index(self.LUT, size)).T
+            rayIJK = np.zeros((size[dim], 3), dtype=np.int16)
+            rayIJK[:,dim] = np.arange(0, size[dim])
+            rayIJK[:,d1] = allIJKs[0,d1]
+            rayIJK[:,d2] = allIJKs[0,d2]
+            linearInds = np.ravel_multi_index((rayIJK[:,0], rayIJK[:,1], 
+                rayIJK[:,2]), size)
+            stride = linearInds[1] - linearInds[0]
+
+            # We now cycle through each voxel in the LUT, check which ray it 
+            # lies on, calculate the indices of all other voxels on this ray, 
+            # load the surface patches for all these voxels and find ray 
+            # intersections to classify the voxel centres. Finally, we remove
+            # the entire set of ray voxels from our copy of the LUT and repeat
+            LUT = copy.deepcopy(self.LUT)
+            while LUT.size: 
+
+                # Where does the ray that passes through this voxel start?
+                # Load all other voxels on this ray
+                startPoint[[d1,d2]] = [allIJKs[0,d1], allIJKs[0,d2]]
+                startInd = np.ravel_multi_index(startPoint.astype(np.int32), size)
+                voxRange = np.arange(startInd, startInd + (size[dim])*stride, 
+                    stride)
+
+                # Romeve those which are present in the LUT / allIJKs arrays
+                keep = np.isin(LUT, voxRange, assume_unique=True, invert=True)
+                LUT = LUT[keep]
+                allIJKs = allIJKs[keep,:]
+                
+                # Load patches along this ray, we can assert that at least 
+                # one patch must be returned. Find intersections 
+                patches = self.toPatchesForVoxels(voxRange)
+                assert patches is not None, 'No patches returned for voxel in LUT'
+                intersectionMus = core._findRayTriangleIntersections2D(
+                    startPoint, patches, dim)
+
+                if not intersectionMus.size:
+                    continue
+                
+                # If intersections were found, perform a parity test. 
+                # Any ray should make an even number of intersections
+                # as it crosses from -ve to +ve infinity
+                if (intersectionMus.shape[0] % 2):
+                    raise RuntimeError("voxelise: odd number of intersections" + 
+                    " found. Does the FoV cover the full extents of the surface?")
+
+                # Calculate points of intersection along the ray. 
+                sorted = np.argsort(intersectionMus)
+                intDs = startPoint[dim] + (intersectionMus[sorted])
+
+                # Assignment. All voxels before the first point of intersection
+                # are outside. The mask is already zeroed for these. All voxels
+                # between point 1 and n could be in or out depending on parity
+                for i in range(1, len(sorted)+1):
+
+                    # Starting from infinity, all points between an odd numbered
+                    # intersection and the next even one are inside the mask 
+                    # Points beyond the last intersection are outside the mask
+                    if ((i % 2) & ((i+1) <= len(sorted))):
+                        indices = ((rayIJK[:,dim] > intDs[i-1]) 
+                            & (rayIJK[:,dim] < intDs[i]))
+                        mask[voxRange[indices]] = 1
+
+            self.voxelised = mask 
+
+        except Exception as e:
+            print("Error voxelising surface.")
+            raise e 
+
 
 
 
