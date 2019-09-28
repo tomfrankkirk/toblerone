16c3dd91e4e0a6914a36b26e654b85700145e4af
Tom <tom@Das-Komputer.local>
2019-09-28 22:59:21 +0100

    typo
    Former-commit-id: beb3f0cfa49221e629ab84460e80aa6b1e1f9604

--- toblerone/core.py
+++ toblerone/core.py
@@ -826,8 +826,8 @@ def _estimateFractionsWorker(surf, supersampler,
     try:
         partialVolumes = np.zeros(len(workerVoxList), dtype=np.float32)
         vox_inds = surf.assocs_keys
-        vox_ijks = np.array(np.unravel_index(vox_inds, surf.index_space.size)).T
-        
+        vox_ijks = np.array(np.unravel_index(vox_inds, surf._index_space.size)).T
+
         for idx, v in enumerate(vox_inds[workerVoxList]):
             partialVolumes[idx] = _estimateVoxelFraction(surf, vox_ijks[idx,:], 
                 v, supersampler)  
