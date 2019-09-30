7feeb7e7eb199b4d3cbd3b3982e82ffc51831120
Tom <tom@Das-Komputer.local>
2019-09-30 21:23:53 +0100

    bringing tests within the module
    Former-commit-id: 1f835f95cbdb795f9ac36401af8f658d1a1faa0b
    Former-commit-id: 035615c0d0751d15d8cd7734142bc5d2386bb157

---
+++ toblerone/tests.py
@@ -0,0 +1,39 @@
+"""Toblerone tests"""
+
+import unittest
+import os.path as op 
+import nibabel 
+import pickle 
+import gzip
+import toblerone
+import numpy as np 
+import multiprocessing
+
+import toblerone.estimators
+
+cores = multiprocessing
+
+class Toblerone_Tests(unittest.TestCase):
+
+    def test_indexing(self):
+        surf = toblerone.Surface('sph.surf.gii')
+        spc = toblerone.ImageSpace('sph_fractions.nii.gz')
+        surf.index_on(spc, np.identity(4))
+
+        with open('sph_indexed.gz', 'r') as f:
+            truth = pickle.load(f)
+
+        self.assertDictEqual(surf.assocs, truth)
+
+    def test_sph(self):
+        surf = toblerone.Surface('sph.surf.gii')
+        spc = toblerone.ImageSpace('sph_fractions.nii.gz')
+        s2r = np.identity(4)
+        supersampler = np.ceil(spc.vox_size / 0.75)
+
+        fracs = toblerone.estimators._structure(surf, spc, 
+            s2r, supersampler, False, multiprocessing.cpu_count)
+
+        truth = nibabel.load('sph_fractions.nii.gz').get_fdata()
+        self.assertTrue(np.testing.assert_array_almost_equal(fracs, truth, 1))
+
