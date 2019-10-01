52119b9610e671cdc0a6216c7107575d8cd09fe3
Tom <tom@Das-Komputer.local>
2019-10-01 22:08:16 +0100

    docs
    Former-commit-id: 8afdc99d4d64dfcd96edefe0579f7821e67fa834
    Former-commit-id: 2eda6e5efc549fff39d7b7a2db97f6c723757124

--- toblerone/__init__.py
+++ toblerone/__init__.py
@@ -1,8 +1,8 @@
 """Surface-based partial volume estimation tools"""
 
-from .main import estimate_all, estimate_cortex, estimate_structure
+from .main import estimate_all, estimate_cortex, estimate_structure, fsl_fs_anat
 from .classes import ImageSpace, Surface
 
 __all__ = ['core', 'classes']
 __author__ = 'Tom Kirk'
-__version__ = '0.1'
+__version__ = '0.3'
