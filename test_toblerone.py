cb76849cdb7927bd46154e93d7ee9ff447e440bb
Tom Kirk <thomas.kirk@eng.ox.ac.uk>
2020-02-04 16:00:39 +0000

    create local copy of objects before apply transform
    Former-commit-id: 5b0a436a21d0e8a8b9b982f02ee5250585355ed0
    Former-commit-id: c97c08140984aaeae93409c39a6cb1c411467949

--- test_toblerone.py
+++ test_toblerone.py
@@ -62,9 +62,9 @@ def test_projection():
     spc = toblerone.ImageSpace(op.join(td, 'ref.nii.gz'))
     sdata = np.ones(ins.points.shape[0], dtype=np.float32)
     vdata = np.ones(spc.size.prod(), dtype=np.float32)
-    sproj = projection.surf2vol_weights(ins, outs, spc, 5, 1)
-    set_trace()
-    # assert (np.abs(1 - vdata[vdata > 0]) < 1e-6).all(), 'surf did not map to ones'
+    # sproj = projection.surf2vol_weights(ins, outs, spc, 3, 1)
+    vproj = projection.vol2surf_weights(ins, outs, spc, 3, 1)
+    assert (np.abs(1 - vdata[vdata > 0]) < 1e-6).all(), 'surf did not map to ones'
 
 
 if __name__ == "__main__":
