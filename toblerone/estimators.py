3adc7348ea7408c8dc3e0096f6d0cbbd298ae32d
Tom Kirk <thomas.kirk@eng.ox.ac.uk>
2020-02-04 16:00:39 +0000

    create local copy of objects before apply transform
    Former-commit-id: 226a70ffa0545c807a220d310a44a1ded2772b6f
    Former-commit-id: 4e92ff7d47de4baea974374ad332e693562a6915

--- toblerone/estimators.py
+++ toblerone/estimators.py
@@ -28,19 +28,18 @@ def _cortex(hemispheres, space, struct2ref, supersampler, cores, ones):
     """
 
     # Create our own local copy of inputs 
-    hemispheres = copy.copy(hemispheres)
-    space = copy.copy(space)
+    loc_hemispheres = copy.deepcopy(hemispheres)
 
-    if not isinstance(hemispheres, list):
-        hemispheres = [hemispheres]
+    if not isinstance(loc_hemispheres, list):
+        loc_hemispheres = [loc_hemispheres]
         
-    surfs = [ s for h in hemispheres for s in h.surfs() ]
+    surfs = [ s for h in loc_hemispheres for s in h.surfs() ]
 
     for s in surfs: 
         s.index_on(space, struct2ref, cores)
 
     # Estimate PV fractions for each surface
-    for h in hemispheres:
+    for h in loc_hemispheres:
         if np.any(np.max(np.abs(h.inSurf.points)) > 
             np.max(np.abs(h.outSurf.points))):
             warnings.warn("Inner surface vertices appear to be further",\
@@ -52,7 +51,7 @@ def _cortex(hemispheres, space, struct2ref, supersampler, cores, ones):
             s._estimate_fractions(supersampler, cores, ones, descriptor)
 
     # Merge the voxelisation results with PVs
-    for h in hemispheres:
+    for h in loc_hemispheres:
         in_pvs = h.inSurf.output_pvs(space).flatten()
         out_pvs = h.outSurf.output_pvs(space).flatten()
 
@@ -65,11 +64,11 @@ def _cortex(hemispheres, space, struct2ref, supersampler, cores, ones):
 
     # Merge the hemispheres, giving priority to GM, then WM, then CSF.
     # Do nothing if just one hemi
-    if len(hemispheres) == 1:
-        outPVs = hemispheres[0].PVs
+    if len(loc_hemispheres) == 1:
+        outPVs = loc_hemispheres[0].PVs
 
     else:
-        h1, h2 = hemispheres
+        h1, h2 = loc_hemispheres
         outPVs = np.zeros((np.prod(space.size), 3), dtype=np.float32)
         outPVs[:,0] = np.minimum(1.0, h1.PVs[:,0] + h2.PVs[:,0])
         outPVs[:,1] = np.minimum(1.0 - outPVs[:,0],
@@ -89,7 +88,7 @@ def _cortex(hemispheres, space, struct2ref, supersampler, cores, ones):
     # Form the surface mask (3D logical) as any voxel containing GM or 
     # intersecting the cortex (these definitions should always be equivalent)
     ctxMask = np.zeros((outPVs.shape[0], 1), dtype=bool)
-    for h in hemispheres:
+    for h in loc_hemispheres:
         for s in h.surfs(): 
             ctxMask[s.reindex_LUT(space)] = True
     ctxMask[outPVs[:,0] > 0] = True 
@@ -120,10 +119,9 @@ def _structure(surf, space, struct2ref, supersampler, ones, cores):
     """
 
     # Create our own local copy of inputs 
-    surf = copy.copy(surf)
-    space = copy.copy(space)
+    loc_surf = copy.deepcopy(surf)
 
-    surf.index_on(space, struct2ref, cores)
-    surf._estimate_fractions(supersampler, cores, ones)
+    loc_surf.index_on(space, struct2ref, cores)
+    loc_surf._estimate_fractions(supersampler, cores, ones)
     
-    return surf.output_pvs(space)
+    return loc_surf.output_pvs(space)
